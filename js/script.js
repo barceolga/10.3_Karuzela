3f74304009815e0dc59e43290d89925f61d3de14
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-03 09:00:41 +0200

    Update content and fix bugs in script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*jshint esversion: 6*/
 $(function() {
-  var $buttons = $('.controls');
+  var $buttonsWrap = $('.controls');
   var $carousel = $('.carousel');
   var $listCarousel = $('.list_carousel');
   var $carouselItem = $('.list_carousel .list_carousel-item');
@@ -12,65 +12,61 @@ $(function() {
   var lastSlide = $listCarousel.find('li:last');
   var slideWidth = $carousel.width();
 
-  $listCarousel.width(slideWidth * carouselLength);
+  $listCarousel.width(slideWidth * (carouselLength + 1));
 
  function setSlide(x) {
-    if (x < 0) x = carouselLength - 1;
-    currentSlide = Math.abs(x) % carouselLength;
+    if (x < 0) {
+      currentSlide = carouselLength - 1;
 
-    if ((x > carouselLength) || (currentSlide !== 0)) {
+      $listCarousel.css({
+        marginLeft: (currentSlide + 1) * slideWidth * -1
+      });
+
+      $listCarousel.animate({
+        marginLeft: (currentSlide) * slideWidth * -1
+      }, 400);
+
+  }else if (x > (carouselLength)) {
+      currentSlide = 1;
+
+      $listCarousel.css({
+      marginLeft: (currentSlide - 1) * slideWidth * -1
+    });
+
       $listCarousel.animate({
        marginLeft: (currentSlide) * slideWidth * -1
-     }, 400);
-   }
+      }, 400);
 
- if (currentSlide === 0) {
-    $listCarousel.css({marginLeft: -400 * currentSlide});
+   } else  {
+    $listCarousel.animate({
+      marginLeft: (x) * slideWidth * -1
+    }, 400);
+    currentSlide = x;
   }
+  $buttons.css("background-color", "transparent");
+  $buttons.eq(currentSlide-1).css('background-color', '#ff8533' );
 }
 
-  for(let x = 0; x < carouselLength; x++) {
-    $buttons.append("<button></button>");
+  for (let x = 0; x < carouselLength; x++) {
+    $buttonsWrap.append("<button></button>");
   }
 
-  $buttons =$('.controls button');
+  var $buttons =$('.controls button');
 
   $buttons.each((index, elem) => {
       $(elem).click(() => {setSlide(index + 1);
-        $buttons.css('background-color', 'transparent');
-        $(elem).css('background-color', '#ff8533');
     });
 
   });
 
   firstSlide.before(lastSlide.clone(true).appendTo($listCarousel));
-  lastSlide.after(firstSlide.clone(true).appendTo($listCarousel));
-  //(firstSlideClone = lastSlide.after(firstSlide.clone(true).appendTo($listCarousel));
-  //lastSlideClone = firstSlide.before(lastSlide.clone(true).appendTo($listCarousel));
 
   $arrowRight.click(() => {
       setSlide(currentSlide + 1);
-    $buttons.css('background-color', 'transparent');
   });
 
   $arrowLeft.click(() => {
     setSlide(currentSlide - 1);
-    /*if (currentSlide === 0) {
-        $listCarousel.css({marginLeft: -1600});
-    }*/
-    $buttons.css('background-color', 'transparent');
   });
 
-  var fourthSlide = $listCarousel.find("li:nth-child(4)");
-  var thirdSlide  = $listCarousel.find("li:nth-child(3)");
-  var fithSlide = $listCarousel.find("li:nth-child(5)");
-  //console.log(thirdSlide);
-   //console.log(fourthSlide);
-   console.log(fithSlide);
-   console.log(lastSlide);
-  // console.log(firstSlideClone);
-
-   $("li").click(function(){
-        console.log($(this).index());
-    });
 });
